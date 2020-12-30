3c931419159daf2c02a4adfc2d6f8a75cee364a1
viniclefer <viniciusclementefernandes@gmail.com>
2020-12-30 10:32:24 -0300

    Create instance operation

--- src/main/java/com/vfcode/cursomc/CursomcApplication.java
+++ src/main/java/com/vfcode/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.vfcode.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vfcode.cursomc.domain.Category;
+import com.vfcode.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+				
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
