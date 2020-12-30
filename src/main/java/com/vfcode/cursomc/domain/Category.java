0bcf314ea51783f7c237fa1328d6e767aa3165cf
viniclefer <viniciusclementefernandes@gmail.com>
2020-12-30 14:28:21 -0300

    association Many to many

--- src/main/java/com/vfcode/cursomc/domain/Category.java
+++ src/main/java/com/vfcode/cursomc/domain/Category.java
@@ -1,11 +1,14 @@
 package com.vfcode.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Category implements Serializable {
@@ -15,6 +18,9 @@ public class Category implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String name;
+  
+  @ManyToMany(mappedBy="categories")
+  private List<Product> products = new ArrayList<>();
 
   public Category() {
   }
@@ -25,33 +31,29 @@ public class Category implements Serializable {
     this.name = name;
   }
 
-  /**
-   * @return Integer return the id
-   */
   public Integer getId() {
       return id;
   }
 
-  /**
-   * @param id the id to set
-   */
   public void setId(Integer id) {
       this.id = id;
   }
 
-  /**
-   * @return String return the name
-   */
   public String getName() {
       return name;
   }
 
-  /**
-   * @param name the name to set
-   */
   public void setName(String name) {
       this.name = name;
   }
+  
+  public List<Product> getProducts() {
+		return products;
+	}
+	
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
 
   @Override
   public int hashCode() {
@@ -77,6 +79,8 @@ public class Category implements Serializable {
       return false;
     return true;
   }
+
+	
 
   
 
